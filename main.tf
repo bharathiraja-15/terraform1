0efb992c4329f962a06d318a66fc5848aa99af3b
bharathiraja-15 <bharathimtech11@gmail.com>
2025-11-12 17:11:29 +0530

    Update EC2 instance subnet and security group

--- main.tf
+++ main.tf
@@ -1,95 +1,11 @@
-provider "aws" {
-  region = "ap-south-1"
-}
-
-# Create VPC
-resource "aws_vpc" "main_vpc" {
-  cidr_block = "10.0.0.0/16"
-  tags = {
-    Name = "Terraform-VPC"
-  }
-}
-
-# Create Subnet
-resource "aws_subnet" "public_subnet" {
-  vpc_id                  = aws_vpc.main_vpc.id
-  cidr_block              = "10.0.1.0/24"
-  map_public_ip_on_launch = true
-  availability_zone       = "ap-south-1a"
-
-  tags = {
-    Name = "Terraform-Subnet"
-  }
-}
-
-# Create Internet Gateway
-resource "aws_internet_gateway" "gw" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  tags = {
-    Name = "Terraform-Gateway"
-  }
-}
-
-# Route Table
-resource "aws_route_table" "public_rt" {
-  vpc_id = aws_vpc.main_vpc.id
-
-  route {
-    cidr_block = "0.0.0.0/0"
-    gateway_id = aws_internet_gateway.gw.id
-  }
-
-  tags = {
-    Name = "Public-Route-Table"
-  }
-}
-
-# Associate Route Table with Subnet
-resource "aws_route_table_association" "a" {
-  subnet_id      = aws_subnet.public_subnet.id
-  route_table_id = aws_route_table.public_rt.id
-}
-
-# Security Group (allow SSH + HTTP)
-resource "aws_security_group" "allow_web" {
-  name   = "allow_web"
-  vpc_id = aws_vpc.main_vpc.id
-
-  ingress {
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  ingress {
-    from_port   = 80
-    to_port     = 80
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
-    from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_web"
-  }
-}
-
-# EC2 Instance
 resource "aws_instance" "public_instance" {
   ami                    = "ami-0c7d68785ec07306c"
   instance_type          = "t3.micro"
-  subnet_id              = aws_subnet.public_subnet.id
-  vpc_security_group_ids = [aws_security_group.allow_web.id]
+
+  subnet_id              = "subnet-0d91e8fd1fa2524c9"
+  vpc_security_group_ids = ["sg-0a9dbf56ebd34521a"]
 
   tags = {
-    Name = "Terraform-EC2"
+    Name = "Terraform-plugin"
   }
 }
